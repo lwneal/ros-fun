3392678e50bc38583711a48d33cac4ed5e36df87
Larry Neal <nealla@lwneal.com>
2016-12-08 18:29:56 -0800

    Add Resnet experiments
    It takes ~30 seconds per frame, but we can run Resnet over every
    bounding box in the image to get accurate class labels.
    
    Maybe we can use these as training data for the other network?

---
+++ resnet/resnet.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import numpy as np
+from resnet50 import ResNet50
+from keras.preprocessing import image
+from imagenet_utils import preprocess_input, decode_predictions
+
+if len(sys.argv) < 2:
+    print("Usage: {} image.jpg".format(sys.argv[0]))
+    exit()
+img_path = sys.argv[1]
+
+start_time = time.time()
+print "loading resnet..."
+model = ResNet50(weights='imagenet')
+print "finished loading resnet in {} seconds".format(time.time() - start_time)
+
+start_time = time.time()
+print "loading image"
+
+WIDTH=224
+
+# 414 backpack
+# 423 barber_chair
+# 559 folding_chair
+# 624 library
+# 765 rocking_chair
+CLASS = 423
+
+full_image = image.load_img(img_path, target_size=(WIDTH*4, WIDTH*4))
+
+resnet_output = np.zeros((WIDTH*4, WIDTH*4), dtype=float)
+
+start_time = time.time()
+STEP = 8
+for dx in range(0, WIDTH*4 - WIDTH/2, STEP):
+    for dy in range(0, WIDTH*4 - WIDTH/2, STEP):
+        # View 25% of the image at a time
+        img = full_image.crop((dx, dy, dx + WIDTH, dy + WIDTH))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        preds = model.predict(x)
+
+        pos_x = dx + WIDTH/2
+        pos_y = dy + WIDTH/2
+        for i in range(STEP):
+            for j in range(STEP):
+                resnet_output[pos_y + i][pos_x + j] = preds[0][CLASS]
+        print dx, dy, preds[0][CLASS]
+
+from PIL import Image
+resnet_output *= 255.0 / resnet_output.max()
+img = Image.fromarray(resnet_output).convert('RGB')
+img.save('output.jpg')
