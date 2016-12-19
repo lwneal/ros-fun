95513298c42d79833a41684c9563aca3aa1b0d01
Larry Neal <nealla@lwneal.com>
2016-12-19 12:23:54 -0800

    Run ResNet50 on each frame of video
    Latency remains under 200ms, even over the Internet

--- roscam/neural_network.py
+++ roscam/neural_network.py
@@ -3,8 +3,30 @@ import numpy as np
 import random
 import h5py
 
+from resnet50 import ResNet50
+import imagenet_utils
+
+model = ResNet50(weights='imagenet', include_top=False)
+
+def pixels_to_input(pixels):
+    x = pixels.astype(np.float)
+    # Expected input shape: (batch_size x channels x height x width)
+    x = np.transpose(x, (2, 0, 1))
+    x = np.expand_dims(x, axis=0)
+    x = imagenet_utils.preprocess_input(x)
+    return x
+
 def init():
+    print("Resnet warming up, this will take ~30 seconds...")
+    pixels = np.zeros((480, 640, 3))
+    x = pixels_to_input(pixels)
+    preds = model.predict(x)
+    print preds
+    print("Resnet is ready!")
     pass
 
 def run(pixels):
-    return pixels
+    x = pixels_to_input(pixels)
+    preds = model.predict(x)
+    print preds.shape
+    return preds
