c6808fd5d581c1e8593a0f3d8b0d7539778f9df0
Larry Neal <nealla@lwneal.com>
2017-01-09 12:40:53 -0800

    Less verbose logging

--- roscam/human_detector/__init__.py
+++ roscam/human_detector/__init__.py
@@ -17,11 +17,7 @@ def init(model_filename):
 
 
 def detect_human(jpg_data):
-    print("detect_human visual_activations resnet")
     visual_activations = resnet(jpg_data)
-    print("detect_human expand_dims")
     visual_activations = np.expand_dims(visual_activations, axis=0)
-    print("detect_human predict")
     preds = model.predict(visual_activations)
-    print("detect_human reshape")
     return preds.reshape(preds.shape[1:-1])
