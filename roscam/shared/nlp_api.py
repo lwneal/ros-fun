2ab1ddc56517380f746744029d8d9d3c9e416098
Larry Neal <nealla@lwneal.com>
2017-01-18 23:37:29 -0800

    Clip text as well as padding to length

--- roscam/shared/nlp_api.py
+++ roscam/shared/nlp_api.py
@@ -47,11 +47,12 @@ def onehot_to_words(onehot):
 def words_to_onehot(words, pad_to_length=None):
     vectors, indices = words_to_vec(words)
     if pad_to_length:
-        ret = np.zeros((pad_to_length, WORDVEC_DIM))
         v = indices_to_onehot(indices)
-        word_count = v.shape[0]
-        ret[:word_count] = v
-        # Pad right with END_TOKEN
+        word_count = min(v.shape[0], pad_to_length)
+        # Fill from left with up to pad_to_length vectors
+        ret = np.zeros((pad_to_length, WORDVEC_DIM))
+        ret[:word_count] = v[:word_count]
+        # Fill right with END_TOKEN
         ret[word_count:, END_TOKEN_IDX] = 1.0
         return ret
     else:
