592664386a51c47acd6a9a7bf99ea5cd253b9373
Lawrence Neal <nealla@oregonstate.edu>
2017-02-01 12:27:24 -0800

    Add model info output

--- roscam/vision_server/train_image_captioning_mao.py
+++ roscam/vision_server/train_image_captioning_mao.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
+import subprocess
+import json
+import time
 import random
+import socket
 import os
 import sys
 import math
@@ -25,7 +29,7 @@ def get_random_grefexp(reference_key=dataset_grefexp.KEY_GREFEXP_TRAIN):
     return jpg_data, box, text
 
 
-def get_next_example():
+def get_next_example(average_box_context=False):
     while True:
         jpg_data, box, text = get_random_grefexp()
         pixels = util.decode_jpg(jpg_data)
@@ -37,10 +41,10 @@ def get_next_example():
         yield x, y
 
 
-def get_batch(batch_size=10):
+def get_batch(batch_size=10, **kwargs):
     X = []
     Y = []
-    generator = get_next_example()
+    generator = get_next_example(**kwargs)
     for _ in range(batch_size):
         x_i, y_i = next(generator)
         X.append(x_i)
@@ -77,35 +81,73 @@ def demonstrate(model):
     print('Prediction: {}'.format(nlp_api.onehot_to_words(onehot_words)))
 
 
-def train(model):
+def train(model, learning_rate=.05, batch_size=10):
+    training_kwargs = {
+        'average_box_context': False,
+    }
     def generator():
         try:
             while True:
-                yield get_batch()
+                yield get_batch(batch_size, **training_kwargs)
         except Exception as e:
             import traceback
             traceback.print_exc()
-    print("Training start: weights avg: {}".format(model.get_weights()[0].mean()))
-    model.fit_generator(generator(), samples_per_epoch=1000, nb_epoch=1)
-    print("Training end: weights mean {}".format(model.get_weights()[0].mean()))
-    demonstrate(model)
 
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', learning_rate=learning_rate)
+
+    print("Training start: weights avg: {}".format(model.get_weights()[0].mean()))
+    start_time = time.time()
+    history = model.fit_generator(generator(), samples_per_epoch=1000, nb_epoch=1)
+    print("Training end: weights mean {}".format(model.get_weights()[0].mean()))
+
+    info = {
+            'start_time': start_time,
+            'end_time': time.time(),
+            'loss': history.history['loss'][0],
+            'hostname': socket.gethostname(),
+            'dataset': 'dataset_grefexp_train',
+            'learning_rate': learning_rate,
+            'optimizer': 'rmsprop',
+            'batch_size': batch_size,
+            'samples': 1000,
+            'training_kwargs': training_kwargs,
+    }
+    demonstrate(model)
+    return info
+
+def save_model_info(info_filename, info, model_filename):
+    if os.path.exists(info_filename):
+        return
+    info['model_filename'] = model_filename
+    info['history'] = []
+    with open(info_filename, 'w') as fp:
+        fp.write(json.dumps(info, indent=2))
+
+
+def save_training_info(info_filename, info, model_filename):
+    info['checksum'] = subprocess.check_output(['md5sum', model_filename])
+    data = json.load(open(info_filename))
+    data['history'].append(info)
+    with open(info_filename, 'w') as fp:
+        fp.write(json.dumps(data, indent=2))
 
 if __name__ == '__main__':
-    output_filename = sys.argv[1]
-    input_filename = None
-    if len(sys.argv) > 2:
-        input_filename = sys.argv[2]
+    model_filename = sys.argv[1]
+    if os.path.exists(model_filename):
         from keras.models import load_model
-        model = load_model(input_filename)
+        model = load_model(model_filename)
     else:
         model = mao_net.build_model()
 
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', learning_rate=.05)
+    info_filename = model_filename.replace('.h5', '') + '.json'
+    info = {}
+
+    save_model_info(info_filename, info, model_filename)
     resnet.init()
     try:
         while True:
-            train(model)
-            model.save(output_filename)
+            train_info = train(model)
+            save_training_info(info_filename, train_info, model_filename)
+            model.save(model_filename)
     except KeyboardInterrupt:
         print("Stopping due to keyboard interrupt")
